2259f8ab172f0dc331d825de6ee150e0d85a99a7
AbhiR1 <abiravi94@icloud.com>
2022-07-16 01:27:57 +0200

    adding second commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        int i = 1;
-        int k = 0;
+        int l = 0;
+        int k = 2;
 
         for (int j = 0; j <= 3; j++) {
             k = k+j;
+
+            l = l+j;
         }
         System.out.println(k);
+        System.out.println(l);
         System.out.println("Hello world!");
 
 
