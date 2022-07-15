973ddef97f8ffaa0acfc8ef4e8ded7ca51d0e444
AbhiR1 <abiravi94@icloud.com>
2022-07-16 01:02:43 +0200

    Adding first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int k = 0;
+
+        for (int j = 0; j <= 3; j++) {
+            k = k+j;
+        }
+        System.out.println(k);
+        System.out.println("Hello world!");
+
+
+
+
+    }
+}
